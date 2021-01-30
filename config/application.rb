8fa8416a1d588abae6597b4ffe2193ec672122ab
yuta-kamizuka <cybor_ggg@icloud.com>
2021-01-31 01:38:23 +0900

    サインアップページ

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,15 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Todo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    config.i18n.default_locale = :ja
+
       config.generators do |g|
         g.stylesheets false
         g.javascripts false
