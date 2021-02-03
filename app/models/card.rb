ebe3d1199089e55369d253d381bf88bbbee48b5d
yuta-kamizuka <cybor_ggg@icloud.com>
2021-02-03 17:32:50 +0900

    カード作成機能

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,5 +2,5 @@ class Card < ApplicationRecord
   belongs_to :list
 
   validates :title, length: {in: 1..255}
-  validates :memo, length: {maxmum: 1000}
+  validates :memo, length: {maximum: 1000}
 end
